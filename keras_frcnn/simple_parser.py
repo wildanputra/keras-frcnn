753c2d0c9da0b365adf860931548ccc0a3b855a4
Wildan Putra <wildanskater@gmail.com>
2019-08-02 15:11:11 +0700

    Add files via upload
    setting location box from dataset gcp to normal

--- keras_frcnn/simple_parser.py
+++ keras_frcnn/simple_parser.py
@@ -44,8 +44,7 @@ def get_data(input_path):
 				else:
 					all_imgs[filename]['imageset'] = 'test'
 
-			all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
-
+			all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1*cols), 'x2': int(x2*cols), 'y1': int(y1*rows), 'y2': int(y2*rows)})
 
 		all_data = []
 		for key in all_imgs:
